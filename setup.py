afdb4493608e7646bd16550cc85c0bd9db6acd3c
gmonacho <gaelmonach123@live.fr>
2022-07-31 15:12:56 +0200

    packages setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bekindto_utils',
+    requires=[],
+    packages=['bekindto_utils'],
+    description='Bekindto project utils package',
+    long_description='Bekindto project utils package',
+    version='0.1',
+    url='https://github.com/saucisses-a-roulettes/bekindto-utils',
+    author='Saucisses Team',
+    keywords=['pip', 'bekindto', 'utils']
+)
