9314b524f694bdc5ec676a3e9850376082b5444d
gmonacho <gaelmonach123@live.fr>
2022-07-31 20:24:50 +0200

    a lot of things

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='saucisses_utils',
     requires=[],
-    packages=['saucisses_utils'],
+    packages=['saucisses_utils/factories', 'saucisses_utils/models'],
     description='Saucisses Team project utils package',
     long_description='Saucisses Team project utils package',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/saucisses-a-roulettes/saucisses_utils',
     author='Saucisses Team',
     keywords=['pip', 'bekindto', 'utils']
